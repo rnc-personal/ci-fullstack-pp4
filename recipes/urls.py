9604957c9cd637d92cc44cd9ef977c97265906d1
rnc-personal <richard.challenger@protonmail.com>
2023-04-15 12:14:49 +0000

    setup detail view/urls. template loads, missing css

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.RecipeListView.as_view(), name='home')
+    path('', views.RecipeListView.as_view(), name='home'),
+    path('<slug:slug>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Recipe
 
@@ -13,7 +13,7 @@ class RecipeListView(generic.ListView):
 
 
 class RecipeDetailView(View):
-    def get(self, slug, *args, **kwargs):
+    def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by('-created_date')
