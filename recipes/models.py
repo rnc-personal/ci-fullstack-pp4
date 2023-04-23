dd5cbff9605d3a50751b6eb05fb993059d3f1823
rnc-personal <richard.challenger@protonmail.com>
2023-04-23 15:43:37 +0000

    add rating to comment class and updated form

--- recipes/models.py
+++ recipes/models.py
@@ -60,6 +60,7 @@ class Comment(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     approved = models.BooleanField(default=False)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], default=5)
 
     class Meta:
         ordering = ['-created_date']
