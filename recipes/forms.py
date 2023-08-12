fe3a8042e8d4cf49abb41e9acb949f63cbc8a215
rnc-personal <richard.challenger@protonmail.com>
2023-08-12 09:24:57 +0000

    ADD: Setup Recipe Form Fields

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, Recipe
 from django import forms 
 
 
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body', 'rating',]
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title', 'snippet', 'difficulty', 'image', 'instructions', 'ingredients','cooking_time_minutes',] 
