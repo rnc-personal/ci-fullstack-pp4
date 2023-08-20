0ce47fa08cb65d7210b48411e7c7a035570844a5
rnc-personal <richard.challenger@protonmail.com>
2023-08-20 10:17:13 +0000

    FIX: Broken Homepage Link

--- baking_mama/settings.py
+++ baking_mama/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ci-pp4-baking.herokuapp.com", "localhost", "8000-rncpersonal-cifullstack-rbwodgwpwfa.ws-eu103.gitpod.io"]
+ALLOWED_HOSTS = ["ci-pp4-baking.herokuapp.com", "localhost", "8000-rncpersonal-cifullstack-rbwodgwpwfa.ws-eu104.gitpod.io"]
 
 
 # Application definition
