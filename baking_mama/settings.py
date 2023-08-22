8fbeefe5adb62cd084b053934c28bb7bff44bf26
rnc-personal <richard.challenger@protonmail.com>
2023-08-22 19:51:21 +0000

    Disabling Debug For Build

--- baking_mama/settings.py
+++ baking_mama/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ci-pp4-baking.herokuapp.com", "localhost", "8000-rncpersonal-cifullstack-rbwodgwpwfa.ws-eu104.gitpod.io"]
 
